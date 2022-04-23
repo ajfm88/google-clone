35cbf02a7eb6aadc32e47bdfa79b98025d632d33
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-04-23 12:36:05 -0700

    13. Create pagination component

--- pages/search.js
+++ pages/search.js
@@ -9,24 +9,25 @@ export default function search({results}) {
   const router = useRouter()
   return (
     <div>
-      <Head>
-        <title>{router.query.term} - Search page</title>
-      </Head>
+        <Head>
+            <title>{router.query.term} - Search page</title>
+        </Head>
 
-      {/* Search Header */}
-      <SearchHeader/>
+        {/* Search Header */}
+            <SearchHeader/>
 
-      {/* Search Results */}
+        {/* Search Results */}
 
-      <SearchResults results={results}/>
+          <SearchResults results={results}/>
     </div>
   )
 }
 
 export async function getServerSideProps(context){
-  const mockData = true
+  const startIndex = context.query.start || "1"
+  const mockData = false
   const data = mockData ? Response : await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}&start=${startIndex}`
   ).then((response)=>response.json())
   return {
     props:{
